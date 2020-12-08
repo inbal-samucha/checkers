059f97b29bcb957acfd3a2d7b9b01914bdfe8e6c
inbal-samucha <70012790+inbal-samucha@users.noreply.github.com>
2020-12-08 17:34:31 +0200

    Create progrm.cs

---
+++ progrm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Damka
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game m_Game = new Game();
+            m_Game.Run();
+
+        }
+    }
+}
